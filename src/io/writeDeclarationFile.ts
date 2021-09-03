2593295c6e2f1e1eae7e8d71ead9e50b9addc3d4
worstpractice <54796463+worstpractice@users.noreply.github.com>
2021-09-03 17:53:27 +0200

    1.0.8

--- src/io/writeDeclarationFile.ts
+++ src/io/writeDeclarationFile.ts
@@ -1,6 +1,6 @@
 import { rm, writeFile } from 'fs/promises';
-import { RM_OPTIONS } from '../constants/RM_OPTIONS';
-import { WRITE_OPTIONS } from '../constants/WRITE_OPTIONS';
+import { RM_OPTIONS } from '../constants/RM_OPTIONS.js';
+import { WRITE_OPTIONS } from '../constants/WRITE_OPTIONS.js';
 
 export const writeDeclarationFile = async (writeToPath: string, moduleAugmentation: string): Promise<void> => {
   await rm(writeToPath, RM_OPTIONS);
